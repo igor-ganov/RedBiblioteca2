18ba0407edcccb5d13952c3bc24989a25f5cd9b2
PForever <igor_ganov@live.ru>
2025-01-25 00:30:51 +0100

    standalone migration 3/3

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,16 @@
 import {Component, inject} from '@angular/core';
 import {PermissionService} from '@common/permission-system/UserService';
+import { TopBarComponent } from './layout/top-bar/top-bar.component';
+import { SideMenuComponent } from './layout/side-menu/side-menu.component';
+import { MainComponent } from './layout/main/main.component';
+import { FooterComponent } from './layout/footer/footer.component';
+import { AsyncPipe } from '@angular/common';
 
 @Component({
     selector: 'app-root',
     templateUrl: './app.component.html',
     styleUrl: './app.component.css',
-    standalone: false
+    imports: [TopBarComponent, SideMenuComponent, MainComponent, FooterComponent, AsyncPipe]
 })
 export class AppComponent {
   private readonly permissionService: PermissionService = inject(PermissionService);
--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,31 @@
 import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 
-import { AppModule } from '@app/app.module';
 
-platformBrowserDynamic().bootstrapModule(AppModule, {
-  ngZoneEventCoalescing: true
+import { DatePipe, CommonModule, NgOptimizedImage } from '@angular/common';
+import { provideHttpClient, withFetch } from '@angular/common/http';
+import { provideClientHydration, BrowserModule, bootstrapApplication } from '@angular/platform-browser';
+import { provideAnimations } from '@angular/platform-browser/animations';
+import { AppRoutingModule } from './app/modules/app-routing.module';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { MatButtonModule } from '@angular/material/button';
+
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatMenuModule } from '@angular/material/menu';
+import { MatIconModule } from '@angular/material/icon';
+import { MatInputModule } from '@angular/material/input';
+import { MatDialogModule } from '@angular/material/dialog';
+import { RouterOutlet } from '@angular/router';
+import { AppComponent } from './app/app.component';
+import { importProvidersFrom } from '@angular/core';
+
+bootstrapApplication(AppComponent, {
+    providers: [
+        importProvidersFrom(AppRoutingModule, BrowserModule, CommonModule, FormsModule, MatButtonModule, MatFormFieldModule, MatMenuModule, MatIconModule, MatInputModule, MatButtonModule, MatDialogModule, ReactiveFormsModule, RouterOutlet, NgOptimizedImage),
+        DatePipe,
+        provideHttpClient(withFetch()),
+        // provideRouter(rootRoute),
+        provideClientHydration(),
+        provideAnimations()
+    ]
 })
   .catch(err => console.error(err));
