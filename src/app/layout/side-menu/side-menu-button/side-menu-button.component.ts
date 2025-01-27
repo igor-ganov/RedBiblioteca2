15c7b482192458c376d876f8ac55ff576dcb1e12
PForever <igor_ganov@live.ru>
2025-01-27 13:09:14 +0100

    output() migration

--- src/app/layout/side-menu/side-menu-button/side-menu-button.component.ts
+++ src/app/layout/side-menu/side-menu-button/side-menu-button.component.ts
@@ -1,24 +1,20 @@
-import {Component, HostListener, inject, ViewChild} from '@angular/core';
+import {Component, HostListener, inject, viewChild} from '@angular/core';
 import {SideMenuService} from '../services/SideMenuService';
 import {MatButton} from '@angular/material/button';
-import { MatIcon } from '@angular/material/icon';
+import {MatIcon} from '@angular/material/icon';
 
 @Component({
-    selector: 'app-side-menu-button',
-    templateUrl: './side-menu-button.component.html',
-    styleUrl: './side-menu-button.component.css',
-    imports: [MatButton, MatIcon]
+  selector: 'app-side-menu-button',
+  templateUrl: './side-menu-button.component.html',
+  styleUrl: './side-menu-button.component.css',
+  imports: [MatButton, MatIcon]
 })
 export class SideMenuButtonComponent {
-  @ViewChild('menuButton') menu!: MatButton;
+  public readonly menu = viewChild.required<MatButton>('menuButton');
   public readonly sideMenuService = inject(SideMenuService);
 
-  constructor() {
-
-  }
-
   @HostListener('window:click', ['$event'])
-  closeMenuIfClickAway(e: Event) {
-    if (this.sideMenuService.opened && !this.menu._elementRef.nativeElement.contains(e.target)) this.sideMenuService.opened = false;
+  public closeMenuIfClickAway(e: Event) {
+    if (this.sideMenuService.opened && !this.menu()._elementRef.nativeElement.contains(e.target)) this.sideMenuService.opened = false;
   }
 }
