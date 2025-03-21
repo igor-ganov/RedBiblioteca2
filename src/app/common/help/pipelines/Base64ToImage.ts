818386caf026c87a91a9c48ccb9194378198cf6b
PForever <igor_ganov@live.ru>
2025-03-21 12:51:02 +0100

    added autofix

--- src/app/common/help/pipelines/Base64ToImage.ts
+++ src/app/common/help/pipelines/Base64ToImage.ts
@@ -1,19 +1,21 @@
-import { Pipe, PipeTransform, inject } from '@angular/core';
-import { DomSanitizer, SafeResourceUrl } from '@angular/platform-browser';
+import {inject, Pipe, PipeTransform} from '@angular/core';
+import {DomSanitizer, SafeResourceUrl} from '@angular/platform-browser';
 
 
-@Pipe({ name: 'base64toImage' })
+@Pipe({name: 'base64toImage'})
 export class Base64ToImage implements PipeTransform {
-    private readonly _sanitizer = inject(DomSanitizer);
-    transform(value: string | undefined): SafeResourceUrl | undefined {
-        return value === undefined ? undefined : this._sanitizer.bypassSecurityTrustResourceUrl(value);
-    }
+  private readonly _sanitizer = inject(DomSanitizer);
+
+  public transform(value: string | undefined): SafeResourceUrl | undefined {
+    return value === undefined ? undefined : this._sanitizer.bypassSecurityTrustResourceUrl(value);
+  }
 }
 
-@Pipe({ name: 'base64toStyle' })
+@Pipe({name: 'base64toStyle'})
 export class Base64ToStyle implements PipeTransform {
-    private readonly _sanitizer = inject(DomSanitizer);
-    transform(value: string | undefined): SafeResourceUrl | undefined {
-        return value === undefined ? undefined : this._sanitizer.bypassSecurityTrustStyle(`url("${value}")`);
-    }
+  private readonly _sanitizer = inject(DomSanitizer);
+
+  public transform(value: string | undefined): SafeResourceUrl | undefined {
+    return value === undefined ? undefined : this._sanitizer.bypassSecurityTrustStyle(`url("${value}")`);
+  }
 }
