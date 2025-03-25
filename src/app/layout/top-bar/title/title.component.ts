00d60da3e1a46aaf54eae8b2d6c86a891d36b2c4
PForever <igor_ganov@live.ru>
2025-03-25 15:24:31 +0100

    some cleanup

--- src/app/layout/top-bar/title/title.component.ts
+++ src/app/layout/top-bar/title/title.component.ts
@@ -1,14 +1,17 @@
 import {ChangeDetectionStrategy, Component, inject} from '@angular/core';
 import {TextHost} from '@common/lang-system/TextHost';
 import {RouteService} from '@common/routes/RouteService';
-import {filter, map, of, switchMap} from 'rxjs';
+import {map, of, switchMap} from 'rxjs';
 import {AsyncPipe} from '@angular/common';
+import {RouteData} from "@common/routes/routes";
 
 @Component({
   selector: 'app-title',
   template: `
-@if (title | async; as title) {<h1 class="header">{{title}}</h1>}
-`,
+    @if (title | async; as title) {
+      <h1 class="header">{{ title }}</h1>
+    }
+  `,
   styleUrl: './title.component.css',
   imports: [AsyncPipe],
   changeDetection: ChangeDetectionStrategy.OnPush,
@@ -17,8 +20,10 @@ export class TitleComponent {
   private routeService = inject(RouteService);
   private textHost = inject(TextHost);
   public readonly title = this.routeService.activatedRoute$.pipe(
-    filter(r => r.component !== undefined),
-    switchMap(r => this.textHost.getMenuItem(r.component!) ?? of(undefined)),
+    switchMap(a => a.data),
+    map(d => d as RouteData),
+    map(d => d.textTag),
+    switchMap(r => this.textHost.getMenuItem(r) ?? of(undefined)),
     map(i => i?.title)
   );
 }
